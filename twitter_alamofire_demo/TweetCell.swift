f9ac33b6415be046d72c83b0dede641b5d7bb14c
Chavane Minto <chevy@fb.com>
2017-07-05 22:36:59 -0700

    Added outlets and actions for the Retweet and Favorite buttons. On click, the buttons turn their respective color, the counter in incremented and the user can favorite or retweet a tweet. Labels display retweet and favorite count. [PROBLEM] Favorite count for retweeted tweets not showing.

--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -21,6 +21,14 @@ class TweetCell: UITableViewCell {
     
     @IBOutlet weak var tweetTextLabel: UILabel!
     
+    @IBOutlet weak var RTLabel: UILabel!
+    
+    @IBOutlet weak var favLabel: UILabel!
+    
+    @IBOutlet weak var RTButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
     var tweet: Tweet! {
         didSet {
             profileImage.af_setImage(withURL: URL(string: tweet.user.imageURL)!)
@@ -32,8 +40,91 @@ class TweetCell: UITableViewCell {
             tweetTextLabel.text = tweet.text
             
             timeStampLabel.text = tweet.createdAtString
+            
+            if (tweet.retweetCount == 0)
+            {
+                RTLabel.text = " "
+            } else {
+                RTLabel.text = String(tweet.retweetCount)
+            }
+            
+            if (tweet.favoriteCount == 0)
+            {
+                favLabel.text = " "
+            } else {
+                favLabel.text = String(tweet.favoriteCount)
+            }
         }
     }
+    
+    @IBAction func didTapFavorite(_ sender: Any) {
+        if (favButton.isSelected != true) {
+            
+            favButton.isSelected = true
+            tweet.favorited = true
+            tweet.favoriteCount += 1
+            self.favLabel.text = String(tweet.favoriteCount)
+            
+            //APIManager Request from Cell
+            APIManager.shared.favorite(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                }
+            }
+        } else if (favButton.isSelected == true) {
+            favButton.isSelected = false
+            tweet.favorited = false
+            tweet.favoriteCount -= 1
+            self.favLabel.text = String(tweet.favoriteCount)
+            
+            //APIManager Request from Cell
+            APIManager.shared.unfavorite(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                }
+            }
+        }
+    }
+    
+    
+    @IBAction func didTapRT(_ sender: Any) {
+        if (RTButton.isSelected != true) {
+            
+            RTButton.isSelected = true
+            tweet.retweeted = true
+            tweet.retweetCount += 1
+            self.RTLabel.text = String(tweet.retweetCount)
+            
+            //APIManager Request from Cell
+            APIManager.shared.retweet(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                }
+            }
+        } else if (RTButton.isSelected == true) {
+            RTButton.isSelected = false
+            tweet.retweeted = false
+            tweet.retweetCount -= 1
+            self.RTLabel.text = String(tweet.retweetCount)
+            
+            //APIManager Request from Cell
+            APIManager.shared.unretweet(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                }
+            }
+        }
+
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
