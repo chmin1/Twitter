1c6bc57532d2891b7846d0a255dc36c13b17eac6
Chavane Minto <chevy@fb.com>
2017-07-07 16:51:12 -0700

    Added functionality to the View controller for buttons. Displays all tweet data. Not yet configured to show tweet replies.

--- twitter_alamofire_demo/DetailViewController.swift
+++ twitter_alamofire_demo/DetailViewController.swift
@@ -7,13 +7,140 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    @IBOutlet weak var screenNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetLabel: UILabel!
+    
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+    @IBOutlet weak var RTLabel: UILabel!
+    
+    @IBOutlet weak var favLabel: UILabel!
+    
+    @IBOutlet weak var RTButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    var tweet: Tweet!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let tweet = tweet {
+            profileImage.af_setImage(withURL: URL(string: tweet.user.imageURL)!)
+            
+            authorLabel.text = tweet.user.name
+            
+            screenNameLabel.text = "@\(tweet.user.screenName)"
+            
+            tweetLabel.text = tweet.text
+            
+            timestampLabel.text = tweet.createdAtString
+            
+            if (tweet.retweetCount == 0)
+            {
+                RTLabel.text = " "
+            } else {
+                RTLabel.text = String(tweet.retweetCount)
+            }
+            
+            if (tweet.favoriteCount == 0)
+            {
+                favLabel.text = " "
+            } else {
+                favLabel.text = String(tweet.favoriteCount)
+            }
+            
+            if (tweet.retweeted == true)
+            {
+                RTButton.isSelected = true
+            } else {
+                RTButton.isSelected = false
+            }
+            
+            if (tweet.favorited == true)
+            {
+                favButton.isSelected = true
+            } else {
+                favButton.isSelected = false
+            }
+
+        }
+    }
+    
+    @IBAction func didFav(_ sender: Any) {
+        if (favButton.isSelected != true) {
+            
+            favButton.isSelected = true
+            tweet.favorited = true
+            tweet.favoriteCount += 1
+            self.favLabel.text = String(tweet.favoriteCount)
+            
+            //APIManager Request from Cell
+            APIManager.shared.favorite(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                }
+            }
+        } else if (favButton.isSelected == true) {
+            favButton.isSelected = false
+            tweet.favorited = false
+            tweet.favoriteCount -= 1
+            self.favLabel.text = String(tweet.favoriteCount)
+            
+            //APIManager Request from Cell
+            APIManager.shared.unfavorite(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                }
+            }
+        }
+    }
+    
+    @IBAction func didRT(_ sender: Any) {
+        if (RTButton.isSelected != true) {
+            
+            RTButton.isSelected = true
+            tweet.retweeted = true
+            tweet.retweetCount += 1
+            self.RTLabel.text = String(tweet.retweetCount)
+            
+            //APIManager Request from Cell
+            APIManager.shared.retweet(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                }
+            }
+        } else if (RTButton.isSelected == true) {
+            RTButton.isSelected = false
+            tweet.retweeted = false
+            tweet.retweetCount -= 1
+            self.RTLabel.text = String(tweet.retweetCount)
+            
+            //APIManager Request from Cell
+            APIManager.shared.unretweet(tweet) { (tweet: Tweet?, error: Error?) in
+                if let  error = error {
+                    print("Error favoriting tweet: \(error.localizedDescription)")
+                } else if let tweet = tweet {
+                    print("Successfully favorited the following Tweet: \n\(tweet.text)")
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
